e862d99eb27f27865eea7fd4aa429c1568bd6e73
sana <sananafari@yahoo.com>
2024-09-22 10:30:00 +0330

    js changed

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-    //Initial Ratings
-    const ratings = {
-        sony: 4.7,
-        samsung: 3.4,
-        vizio: 2.3,
-        panasonic: 3.6,
-        phillips: 4.1
-    }
+// Initial Ratings
+const ratings = {
+    sony: 4.7,
+    samsung: 3.4,
+    vizio: 2.3,
+    panasonic: 3.6,
+    phillips: 4.1
+}
 
-    //Total Stars
-    const starsTotal = 5;
+// Total Stars
+const starsTotal = 5;
 
+// Run getRatings when DOM loads
+document.addEventListener('DOMContentLoaded', getRatings);
 
-    //Run getRating when dom loads
-    document.addEventListener('DOMContentLoaded', getRatings)
+// Get ratings 
+function getRatings() {
+    for (let rating in ratings) {
+        // Get percentage
+        const starPercentage = (ratings[rating] / starsTotal) * 100;
 
-    //Get ratings 
-    function getRatings() {
-      for(let rating in ratings) {
-        //Get percenage
-        const starPercentage = (ratings[rating] / starsTotal) *100;
-
-        //Round to nearest 10
+        // Round to nearest 10
         const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
-        console.log(starPercentageRounded);
-      }
+
+        // Set width of stars inner to percentage 
+        document.querySelector(`.${rating} .stars-inner`).style.width = starPercentageRounded;
     }
+}
